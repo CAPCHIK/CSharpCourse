66d1bf154d9ad2810edec3965478c117589ef616
Maksim <maksalmak@gmail.com>
2016-11-19 17:03:54 +0300

    Material for lession 3

---
+++ Less3/HomeWork/10MinOfFour/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        // Задача 10 минимум четырех
+
+        //Напишите функцию 
+        //static int min (int a, int b, int c, int d) 
+        //находящую наименьшее из четырех данных чисел.
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Less3/HomeWork/12Vote/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        // Задача 12 про голосование
+
+        //Написать метод Vote 
+        //который принимает в сбя неограниченное количество параметров типа bool
+        //возвращать метод должен значение bool
+        //метод возвращает true, если в переданных параметрах больше true, чем false
+        //иначе - возвращает false
+
+        //Если небыло переданно параметров, метод должен вернуть false
+
+        //Например, если вызвать 
+        //bool result = Vote(true, true, false);
+
+        //result должно хранить true
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
