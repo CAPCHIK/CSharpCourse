1a2844d6ed7844ea2c693f95d665e3df7b7912c5
Maksim <maksalmak@gmail.com>
2016-11-30 22:34:33 +0300

    Lesson18

--- Less4/HomeWork/18NameSpace/Program.cs
+++ Less4/HomeWork/18NameSpace/Program.cs
@@ -4,6 +4,11 @@ namespace ConsoleApplication
 {
     public class Program
     {
+
+        //Укажите во всех своих домашних заданиях правильные пространства имён
+        //(namespace)
+        //Первым словом в namespace должно быть название папки, в которой вы ведете всю работу с 
+        //данным курсом
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
